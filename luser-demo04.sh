a8c666b52b058b9bc426016623dfe82f60d48322
Sbtah <shisastore@protonmail.com>
2023-03-12 19:49:25 +0100

    Added script for user creation.

--- luser-demo04.sh
+++ luser-demo04.sh
@@ -5,11 +5,19 @@
 
 
 # 1 Ask for the user name.
-
+read -p 'Enter the username to create: ' USER_NAME
 
 # 2 Ask for the real name.
-
+read -p 'Enter the name of the person who this account is for: ' COMMENT
 
 # 3 Ask for the password.
+read -p 'Enter the password to use for the account: ' PASSWORD
 
+# 4 Create the user.
+useradd -c "${COMMENT}" -m ${USER_NAME}
 
+# 5 Set the password for the user.
+echo "${USER_NAME}:${PASSWORD}" | chpasswd
+
+# 6 Force password change on the first login.
+passwd -e ${USER_NAME}
