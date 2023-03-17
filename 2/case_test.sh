6d32e8f3dfba316fe83634db6e0ef6fc47f27d12
Sbtah <shisastore@protonmail.com>
2023-03-17 21:01:00 +0100

    Added case statements part.

--- 2/case_test.sh
+++ 2/case_test.sh
@@ -4,21 +4,44 @@
 PS3="What city would you like to visit?: "
 
 
-select city in 'Tokyo' 'London' 'Los Angeles' 'Moscow' 'Dubai' 'Manchester' 'New York' 'Paris' 'Istanbul' 'Milan' 'Abu Dhabi' 'Pune' 'Nairobi' 'Berlin' 'Karachi'
-do
+select city in 'Tokyo' 'London' 'Los Angeles' 'Moscow' 'Dubai' 'Manchester' 'New York' 'Paris' 'Istanbul' 'Milan' 'Abu Dhabi' 'Pune' 'Nairobi' 'Berlin' 'Karachi'; do
   case "${city}" in
-  'Tokyo') echo "You picked a trip to Japan, Great choice!";;
-  'London'|'Manchester') echo "You picked a trip to England, Great choice!";;
-  'Los Angeles'|'New York') echo "You picked a trip to USA, Great choice!";;
-  'Moscow') echo "You picked a trip to Russia, Great choice!";;
-  'Dubai'|'Abu Dhabi') echo "You picked a trip to United Arab Emirates, Great choice!";;
-  'Paris') echo "You picked a trip to France, Great choice!";;
-  'Istanbul') echo "You picked a trip to Turkey, Great choice!";;
-  'Milan') echo "You picked a trip to Italy, Great choice!";;
-  'Pune') echo "You picked a trip to India, Great choice!";;
-  'Nairobi') echo "You picked a trip to Kenya, Great choice!";;
-  'Berlin') echo "You picked a trip to Germany, Great choice!";;
-  'Karachi') echo "You picked a trip to Pakistan, Great choice!";;
+    'Tokyo')
+      country='Japan'
+      ;;
+    'London'|'Manchester')
+      country='England'
+      ;;
+    'Los Angeles'|'New York')
+      country='USA'
+      ;;
+    'Moscow')
+      country='Russia'
+      ;;
+    'Dubai'|'Abu Dhabi')
+      country='United Arab Emirates'
+      ;;    
+    'Paris')
+      country='France'
+      ;;
+    'Istanbul')
+      country='Turkey'
+      ;;
+    'Milan')
+      country='Italy'
+      ;;
+    'Pune')
+      country='India'
+      ;;
+    'Nairobi')
+      country='Kenya'
+      ;;
+    'Berlin')
+      country='Germany'
+      ;;
+    'Karachi')
+      country='Pakistan'
+      ;;
   esac
-  break
+    echo "You picked a trip to ${country}, Great choice!"
 done
