a04787e0dc34727de29ccb0fc5a181b1f1c77235
Sbtah <shisastore@protonmail.com>
2023-03-24 16:18:59 +0100

    Update add-local-user.sh

--- add-local-user.sh
+++ add-local-user.sh
@@ -10,31 +10,31 @@ then
     then
     USER_NAME="${1}"
     shift
-    COMMENT="${@}"
+    COMMENT="$@"
     PASSWORD=$( date +%s%N${RANDOM}${RANDOM}${RANDOM} | sha256sum | head -c 16)
-    useradd -c "${COMMENT}" -m ${USER_NAME}
+    useradd -c "${COMMENT}" -m "${USER_NAME}"
         if [[ "${?}" -eq 0 ]]
             then
                 echo "${USER_NAME}:${PASSWORD}" | chpasswd
                 if [[ "${?}" -eq 0 ]]
                 then
                     echo "User with login: ${USER_NAME} and password: ${PASSWORD} was created for host: $(uname -n)"
-                    passwd -e ${USER_NAME}
+                    passwd -e "${USER_NAME}"
                     exit 0
                 else
-                    echo "Creation of user failed at chpasswd!"
+                    echo "Creation of user failed at chpasswd!" 1>&2
                     exit 1
                 fi
             else
-                echo 'Creation of user failed!'
+                echo 'Creation of user failed!' 1>&2
                 exit 1
             fi
     else
-        echo "Usage: ${0} USER_NAME [FULL_NAME]"
+        echo "Usage: ${0} USER_NAME [FULL_NAME]" 1>&2
         exit 1
     fi
 else
-    echo 'You are not a superuser. Please run with sudo or as a root.'
+    echo 'You are not a superuser. Please run with sudo or as a root.' 1>&2
     exit 1  
 fi
 exit 0
