59bcd0937d6db37290be2dbd79511c08f80a11a2
Sbtah <shisastore@protonmail.com>
2023-03-25 16:10:43 +0100

    Added part on case statements.

---
+++ luser-demo09.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+# This script demostrates the case statement.
+
+
+# if [[ "${1}" = 'start' ]]; then
+#     echo 'Starting.'
+# elif [[ "${1}" = 'stop' ]]; then
+#     echo 'Stopping.'
+# elif [[ "${1}" = 'status' ]]; then
+#     echo 'Status.'
+# else
+#     echo 'Supply a valid option.' 1>&2
+#     exit 1
+# fi
+
+
+# Case:
+
+
+case "${1}" in
+    'start')
+        echo 'Starting.'
+        ;;
+    'stop')
+        echo 'Stopping.'
+        ;;
+    'status'|'state'|'--status'|'--state')
+        echo 'Status'
+        ;;
+    *)
+        echo 'Supply a valid option.' 1>&2
+        exit 1
+        ;;
+esac
